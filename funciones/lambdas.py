16774eee9b054ef5602d99b810b99c820514d2af
Blackstar84 <carlosalmada84@gmail.com>
2023-05-22 09:55:06 -0400

    se agrega los ejemplos de lambdas con parámetros por defecto y otro que recibe tupla *args y diccionario **kwgars

--- funciones/lambdas.py
+++ funciones/lambdas.py
@@ -12,11 +12,13 @@ funcion_grados = lambda grados : grados * 1.8 + 32
 
 print(funcion_grados(5))
 
-"""
-    sin_parametros = lambda : True
-    
-    parametros_default = lambda p1=40, p2=20, p3=60 : p1 + p2 +p3
-    
-    asterisco = lambda *args, **kwargs : len(args) + len(kwargs)
 
-"""
+sin_parametros = lambda : True
+
+parametros_default = lambda p1=40, p2=20, p3=60 : p1 + p2 +p3
+
+asterisco = lambda *args, **kwargs : len(args) + len(kwargs)
+
+print(sin_parametros())
+print(parametros_default())
+print(asterisco(10,20,30,hola=2, saludo='buen día'))
